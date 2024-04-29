5918640069a6eb533f3a8bd2ad7cbf78033390c6
Simone Mugnai <simone.mugnai@yahoo.it>
2024-04-29 06:36:41 +0000

    Update statistics in utils for testing mean and std of  the imputed values

--- lib/utils/statistics.py
+++ lib/utils/statistics.py
@@ -13,32 +13,22 @@ def ensure_list(obj):
 
 
 def prediction_dataframe(y, index, columns=None, aggregate_by='mean'):
-    """Aggregate batched predictions in a single DataFrame.
-
-    @param (list or np.ndarray) y: the list of predictions.
-    @param (list or np.ndarray) index: the list of time indexes coupled with the predictions.
-    @param (list or pd.Index) columns: the columns of the returned DataFrame.
-    @param (str or list) aggregate_by: how to aggregate the predictions in case there are more than one for a step.
-    - `mean`: take the mean of the predictions
-    - `central`: take the prediction at the central position, assuming that the predictions are ordered chronologically
-    - `smooth_central`: average the predictions weighted by a gaussian signal with std=1
-    - `last`: take the last prediction
-    @return: pd.DataFrame df: the evaluation mask for the DataFrame
-    """
-    index = [np.full((y[i].shape[0],), index[i], dtype=index.dtype).tolist() for i in range(len(index))]
-
-    #dfs = [pd.DataFrame(data=data.reshape(data.shape[:2]), index=idx, columns=columns) for data, idx in zip(y, index)]
-    dfs = [pd.DataFrame(data=y[i].reshape(-1, y[i].shape[-1]), index=idx, columns=columns) 
-           for i, idx in enumerate(index)]
-
-    df = pd.concat(dfs)
+    """Aggregate batched predictions in a single DataFrame."""
+    df = pd.DataFrame(data=y, index=index, columns=columns)
     preds_by_step = df.groupby(df.index)
-    # aggregate according passed methods
-    aggr_methods = ensure_list(aggregate_by)
-    dfs = []
-    for aggr_by in aggr_methods:
-        if aggr_by == 'mean':
-            dfs.append(preds_by_step.mean())
-        if aggr_by == 'sd':
-            dfs.append(preds_by_step.std())
-    return dfs
+
+    if aggregate_by == 'mean':
+        df_agg = preds_by_step.mean()
+    elif aggregate_by == 'sd':
+            df_agg= preds_by_step.std()
+    elif aggregate_by == 'central':
+        df_agg = preds_by_step.aggregate(lambda x: x.iloc[len(x) // 2])
+    elif aggregate_by == 'smooth_central':
+        from scipy.signal import gaussian
+        df_agg = preds_by_step.aggregate(lambda x: np.average(x, weights=gaussian(len(x), 1)))
+    elif aggregate_by == 'last':
+        df_agg = preds_by_step.aggregate(lambda x: x.iloc[-1])
+    else:
+        raise ValueError(f'Invalid aggregation method. Choose from mean, central, smooth_central, last.')
+
+    return df_agg
